db8c96a64f530c6799208949fe77b3bee75aa999
Cauê Conte <104505715+CaueConte@users.noreply.github.com>
2022-10-12 16:32:34 -0400

    Add files via upload

---
+++ calculadorapy.py
@@ -0,0 +1,31 @@
+import os
+
+soma = 0
+subtracao = 0
+multiplicacao = 0
+divisao = 0
+numeroa = 0
+numerob = 0
+print("1-Soma")
+print("2-subtracao")
+print("3-Multiplicacao")
+print("4-Divisao")
+
+while True:
+    try:
+        escolha = int(input("Escolha operacao: "))
+        numeroa = int(input("Digite o numero a: "))
+        numerob = int(input("Digite o numero b: "))
+        if escolha == 1:
+            print(numeroa + numerob)
+        elif escolha == 2:
+            print(numeroa - numerob)
+        elif escolha == 3:
+            print(numeroa * numerob)
+        elif escolha == 4:
+            print(numeroa / numerob)
+        else:
+            pass
+    except:
+        print()
+        
